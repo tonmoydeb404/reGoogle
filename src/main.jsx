40003dad620948c7406aa4e3bb136c69b43dc087
Tonmoy Deb <tonmoydeb404@gmail.com>
2022-03-03 02:24:39 +0600

    project setup completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './styles/main.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles/main.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
