23bc167053c0249ce098a825f4b6607290ab3719
Tonmoy Deb <tonmoydeb404@gmail.com>
2022-03-07 20:48:00 +0600

    hover state updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Footer = () => (
     <div className=" bg-slate-900 text-slate-50 py-2 text-sm text-center ">
         <div className="container">
-            made with love by{' '}
+            developed by{' '}
             <a
                 href="https://www.tonmoydeb.com"
                 target="_blank"
                 rel="noreferrer"
-                className="text-blue-600"
+                className="text-blue-600 hover:text-blue-700 duration-200"
             >
                 tonmoy deb
             </a>
